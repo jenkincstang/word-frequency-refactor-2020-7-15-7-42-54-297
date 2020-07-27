8bae34cf0be9f11714eb357f281f8e71c0b0b286
jenkincstang <jenkincs@163.com>
2020-07-27 15:28:31 +0800

    [word frequency][jenkin] refactor: remove try statement and format code in the method getResult  in class WordFrequancyGame

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -6,37 +6,31 @@ import java.time.LocalDateTime;
 public class WordFrequencyGame {
     public String getResult(String inputStr) {
         {
+            String[] words = inputStr.split("\\s+");
 
-            try {
-
-                String[] words = inputStr.split("\\s+");
-
-                Map<String,Integer> wordMap = new HashMap<>();
-                for(String word:words){
-                    if(wordMap.containsKey(word)){
-                        wordMap.put(word,wordMap.get(word)+1);
-                    }else {
-                        wordMap.put(word,1);
-                    }
+            Map<String, Integer> wordMap = new HashMap<>();
+            for (String word : words) {
+                if (wordMap.containsKey(word)) {
+                    wordMap.put(word, wordMap.get(word) + 1);
+                } else {
+                    wordMap.put(word, 1);
                 }
-
-                List<Map.Entry<String, Integer>> wordList = new ArrayList<Map.Entry<String, Integer>>(wordMap.entrySet());
-                wordList.sort(new Comparator<Map.Entry<String, Integer>>() {
-                    @Override
-                    public int compare(Map.Entry<String, Integer> preEntry, Map.Entry<String, Integer> laterEntry) {
-                        return laterEntry.getValue().compareTo(preEntry.getValue());
-                    }
-                });
-
-                StringJoiner joiner = new StringJoiner("\n");
-                for (Map.Entry<String, Integer> entry : wordList) {
-                    String s = entry.getKey() + " " +entry.getValue();
-                    joiner.add(s);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-                return "Calculate Error";
             }
+
+            List<Map.Entry<String, Integer>> wordList = new ArrayList<Map.Entry<String, Integer>>(wordMap.entrySet());
+            wordList.sort(new Comparator<Map.Entry<String, Integer>>() {
+                @Override
+                public int compare(Map.Entry<String, Integer> preEntry, Map.Entry<String, Integer> laterEntry) {
+                    return laterEntry.getValue().compareTo(preEntry.getValue());
+                }
+            });
+
+            StringJoiner joiner = new StringJoiner("\n");
+            for (Map.Entry<String, Integer> entry : wordList) {
+                String s = entry.getKey() + " " + entry.getValue();
+                joiner.add(s);
+            }
+            return joiner.toString();
         }
     }
 }
